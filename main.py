11d97cf1fc4659d6796b4ac322bac548021b276f
MartaFagundez <55920614+MartaFagundez@users.noreply.github.com>
2025-01-15 23:12:10 -0300

    feat: schema validation with Pydantic

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from typing import Union
+from typing import Optional, Union
 from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+# Schema Validation with Pydantic
+class Item(BaseModel):
+    title: str
+    content: str
+    published: bool = True # Valor por defecto (esto tiene el efecto de actuar como un campo opcional)
+    rating: Optional[int] = None # Campo opcional
 
 @app.get("/")
 def read_root():
@@ -13,6 +21,8 @@ def read_item(item_id: int, q: Union[str, None] = None):
 
 # Instrucciones del video-tutorial de Sanjeev Thiyagarajan
 @app.post("/items/new-item")
-def create_item(payload: dict = Body(...)):
-    print(payload)
-    return {f"title: {payload["title"]}, content: {payload["content"]}"}
+def create_item(new_item: Item):
+    print(new_item) # Pydantic model
+    print(new_item.model_dump()) # Generate a dictionary representation of the model, optionally specifying which fields to include or exclude.
+    print(new_item.model_dump_json()) # Generates a JSON representation of the model using Pydantic's to_json method.
+    return { "new_item": new_item}
