a59eb847bd6f5950ab8f15d3ff3e3c06e7fe6541
MartaFagundez <55920614+MartaFagundez@users.noreply.github.com>
2025-01-20 23:47:24 -0300

    refactor: add fake database for post and get tests

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 from typing import Optional, Union
 from fastapi import Body, FastAPI
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
+
+# Fake database
+my_items = [
+    {
+        "id": 1,
+        "title": "Item 1",
+        "content": "Item 1 content",
+        "published": True,
+        "rating": 5
+    },
+    {
+        "id": 2,
+        "title": "Item 2",
+        "content": "Item 2 content",
+        "published": True,
+        "rating": 4
+    }
+]
+
+def find_item(id):
+    for item in my_items:
+        if item["id"] == id:
+            return item
+
 
 # Schema Validation with Pydantic
 class Item(BaseModel):
@@ -15,14 +40,23 @@ class Item(BaseModel):
 def read_root():
     return {"Message": "Hello World Again!!"}
 
+
+@app.get("/items")
+def read_items():
+    return {"data": my_items}
+
+
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int): # Al especificar el tipo, Fastapi parsea y genera validaciones automáticamente
+    item = find_item(item_id)
+    return {"item_detail": item}
+
 
 # Instrucciones del video-tutorial de Sanjeev Thiyagarajan
 @app.post("/items/new-item")
 def create_item(new_item: Item):
     print(new_item) # Pydantic model
-    print(new_item.model_dump()) # Generate a dictionary representation of the model, optionally specifying which fields to include or exclude.
-    print(new_item.model_dump_json()) # Generates a JSON representation of the model using Pydantic's to_json method.
-    return { "new_item": new_item}
+    new_item_dict = new_item.model_dump() # Generate a dictionary representation of the model, optionally specifying which fields to include or exclude.
+    new_item_dict["id"] = randrange(0, 10000)
+    my_items.append(new_item_dict)
+    return { "new_item": new_item_dict}
